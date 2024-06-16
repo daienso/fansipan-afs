b4c998b855cfdadcd33b13ffdac274452a9dac27
linhsolar <linh.truong@daienso.com>
2024-06-16 12:18:52 +0300

    just clean the code a bit

--- src/fansipan_afs/afs_api.py
+++ src/fansipan_afs/afs_api.py
@@ -7,10 +7,13 @@ from fansipan_afs.afs_datamodels import AnalyticsSubject, DataSourceChange, Dete
 
 
 class AFSAPI:
+    """basic api
+    """
     @staticmethod
-    def save_detection_config(checkpoint_ts,analytics_subject,
-                              domain_category,detection_model,
-                              config:dict=None,checkpoint_filename=None):
+    def save_detection_config(checkpoint_ts, analytics_subject,
+                              domain_category, detection_model,
+                              config:dict=None, checkpoint_filename=None):
+        """ save detection config to file"""
         config_checkpoint=DetectionConfig(timestamp=checkpoint_ts,
             analyticsSubject=AnalyticsSubject(analyticsSubjectId=analytics_subject),
             domainCategory=domain_category,
@@ -20,16 +23,17 @@ class AFSAPI:
             checkpoint_filename=f'{analytics_subject}-config-{checkpoint_ts}.json'
         with open(checkpoint_filename,mode="w",encoding="utf-8") as checkpoint_fp:
             json.dump(config_checkpoint.dict(exclude_none=True),checkpoint_fp,indent=4)
+
     @staticmethod
-    def save_datasource_change(checkpoint_ts,analytics_subject,list_datasources,
-                               change_type,checkpoint_filename=None):
+    def save_datasource_change(checkpoint_ts, analytics_subject, list_datasources,
+                               change_type, checkpoint_filename=None):
         """Save changes of data sources
         """
-        config_checkpoint=DataSourceChange(timestamp=checkpoint_ts,
+        config_checkpoint = DataSourceChange(timestamp=checkpoint_ts,
             analyticsSubject=AnalyticsSubject(analyticsSubjectId=analytics_subject),
             dataSources=list_datasources,
             changeType=change_type)
         if checkpoint_filename is None:
-            checkpoint_filename=f'{analytics_subject}-datasource-{checkpoint_ts}.json'
-        with open(checkpoint_filename,mode="w",encoding="utf-8") as checkpoint_fp:
-            json.dump(config_checkpoint.dict(exclude_none=True),checkpoint_fp,indent=4)
+            checkpoint_filename = f'{analytics_subject}-datasource-{checkpoint_ts}.json'
+        with open(checkpoint_filename, mode="w", encoding="utf-8") as checkpoint_fp:
+            json.dump(config_checkpoint.dict(exclude_none=True), checkpoint_fp, indent=4)
--- src/fansipan_afs/gen_afs_schemas.py
+++ src/fansipan_afs/gen_afs_schemas.py
@@ -14,15 +14,15 @@ if __name__ == '__main__':
     parser.add_argument('--output_dir', default=".", help='print schema')
     args = parser.parse_args()
     #create dir if not exist
-    if not Path(args.output_dir).exists():
+    if Path(args.output_dir).exists():
         Path(args.output_dir).mkdir(parents=True,exist_ok=True)
     #create file
-    file1=f'{args.output_dir}/featureinstance_schema_{AFS_VERSION}.json'
+    file1 = f'{args.output_dir}/featureinstance_schema_{AFS_VERSION}.json'
     print(f'FeatureInstance schema into {file1}')
-    with open(file1,mode="w",encoding="utf-8") as instance_fp:
-        json.dump(FeatureInstances.model_json_schema(),instance_fp, indent=4)
-    file2=f'{args.output_dir}/detection_config_schema_{AFS_VERSION}.json'
+    with open(file1, mode="w", encoding="utf-8") as instance_fp:
+        json.dump(FeatureInstances.model_json_schema(), instance_fp, indent=4)
+    file2 = f'{args.output_dir}/detection_config_schema_{AFS_VERSION}.json'
     print(f'DetectionConfig schema into {file2}')
-    with open(file2,mode="w",encoding="utf-8") as config_fp:
-        json.dump(DetectionConfig.model_json_schema(),config_fp, indent=4)
+    with open(file2, mode="w", encoding="utf-8") as config_fp:
+        json.dump(DetectionConfig.model_json_schema(), config_fp, indent=4)
     
