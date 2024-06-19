5d43bc57fcb91d5338d6c4de1aea0bd31106b5be
linhsolar <linh.truong@daienso.com>
2024-06-19 20:51:53 +0300

    update export call

--- tests/test_afs.py
+++ tests/test_afs.py
@@ -16,12 +16,12 @@ def test_instances():
     analyticssubject = AnalyticsSubject(analyticsSubjectId='Fansipan')
     detectionmodel = DetectionModel(
         detectionModelRefId="YOLO")
-    object_detection_ex=ObjectDetectionResult(classname='YOLO.Person', score=0.9)
+    object_detection_ex = ObjectDetectionResult(classname='YOLO.Person', score=0.9)
     detection_result_one = CommonDetectionResult(detectionResult=object_detection_ex
         , additionalData={"source": "test", "meta1": {"submeta": "subvalue"}})
     #detectedresult = DetectedResult(detectedresult=[detectedresult_one])
-    timestamp=datetime.now().timestamp()
-    detection_config=DetectionConfig(timestamp=timestamp,config={"windowlength":10,
+    timestamp = datetime.now().timestamp()
+    detection_config=DetectionConfig(timestamp=timestamp, config={"windowlength":10,
                                       "sampingrate":5})
     featureinstance = FeatureInstance(featureInstanceId=str(uuid.uuid4()),
                                       timestamp=timestamp,
@@ -32,7 +32,7 @@ def test_instances():
                                       detectionConfig=detection_config)
     featureinstances = FeatureInstances(featureInstances=[featureinstance])
     # just a test, model_dump() return a dict of the object
-    print(json.dumps(featureinstances.dict(exclude_none=True), indent=4))
+    print(json.dumps(featureinstances.model_dump(exclude_none=True), indent=4))
 
 if __name__ == '__main__':
     print("Instance example")
